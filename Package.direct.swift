5ca0b01169cdcf443a3457fe37ca02372da98f11
Yuta Saito <kateinoigakukun@gmail.com>
2024-06-26 01:58:07 +0000

    indirect vs direct flavors

---
+++ Package.direct.swift
@@ -0,0 +1,32 @@
+// swift-tools-version: 5.10
+
+import PackageDescription
+
+let package = Package(
+    name: "AppPackage",
+    platforms: [.macOS(.v13)],
+    products: [
+        .executable(
+            name: "codegen",
+            targets: ["codegen"]
+        )
+    ],
+    dependencies: [
+        .package(path: "../ToyboxPackage")
+    ],
+    targets: [
+        .target(
+            name: "AppModule",
+            dependencies: [
+                .product(name: "ToyboxCore", package: "ToyboxPackage"),
+                .product(name: "ToyboxModule", package: "ToyboxPackage")
+            ]
+        ),
+        .executableTarget(
+            name: "codegen",
+            dependencies: [
+                .target(name: "AppModule")
+            ]
+        )
+    ]
+)
--- repro.bash
+++ repro.bash
@@ -4,13 +4,17 @@ set -ue -o pipefail
 set -x
 pwd
 
-rm -rf AppPackage/.build
-cp Debug.step1.swift ToyboxPackage/Sources/ToyboxCore/Debug.swift
+for flavor in direct indirect; do
 
-swift build --package-path ./AppPackage -v | tee step1.log
-
-# Source compatible change
-cp Debug.step2.swift ToyboxPackage/Sources/ToyboxCore/Debug.swift
-
-# Curious link error
-swift build --package-path ./AppPackage -v | tee step2.log
+  rm -rf AppPackage/.build
+  cp Package.$flavor.swift AppPackage/Package.swift
+  cp Debug.step1.swift ToyboxPackage/Sources/ToyboxCore/Debug.swift
+  
+  swift build --package-path ./AppPackage -v | tee step1.$flavor.log
+  
+  # Source compatible change
+  cp Debug.step2.swift ToyboxPackage/Sources/ToyboxCore/Debug.swift
+  
+  # Curious link error
+  swift build --package-path ./AppPackage -v | tee step2.$flavor.log
+done
