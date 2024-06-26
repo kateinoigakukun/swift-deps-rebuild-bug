97baf77e7d861f0a11a439fdb0ae0712792f7640
Yuta Saito <kateinoigakukun@gmail.com>
2024-06-26 01:24:38 +0000

    Record build logs

--- repro.bash
+++ repro.bash
@@ -7,10 +7,10 @@ pwd
 rm -rf AppPackage/.build
 cp Debug.step1.swift ToyboxPackage/Sources/ToyboxCore/Debug.swift
 
-swift build --package-path ./AppPackage
+swift build --package-path ./AppPackage -v | tee step1.log
 
 # Source compatible change
 cp Debug.step2.swift ToyboxPackage/Sources/ToyboxCore/Debug.swift
 
 # Curious link error
-swift build --package-path ./AppPackage
+swift build --package-path ./AppPackage -v | tee step2.log
