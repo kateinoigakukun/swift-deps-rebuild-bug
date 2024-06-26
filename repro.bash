f754f917440b045c5586e678a9ac31d59ca91ede
omochimetaru <omochi.metaru@gmail.com>
2024-06-26 09:43:20 +0900

    comment

--- repro.bash
+++ repro.bash
@@ -11,11 +11,11 @@ cd ..
 
 cd ToyboxPackage/Sources/ToyboxCore
 
-# This change is source compatible
+# Source compatible change
 sed 's/a: Int = 0/a: Int = 0, b: Int = 0/' Debug.swift > Debug.2.swift
 mv -f Debug.2.swift Debug.swift
 
 cd ../../../AppPackage
 
-# Curios link error
+# Curious link error
 swift build
