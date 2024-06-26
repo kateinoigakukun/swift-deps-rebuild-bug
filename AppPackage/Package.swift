20f6ba20535012e9ae840d2092db802fa0a70379
Yuta Saito <kateinoigakukun@gmail.com>
2024-06-26 01:44:29 +0000

    Reduce

--- AppPackage/Package.swift
+++ AppPackage/Package.swift
@@ -6,14 +6,6 @@ let package = Package(
     name: "AppPackage",
     platforms: [.macOS(.v13)],
     products: [
-        .library(
-            name: "AppModule",
-            targets: ["AppModule"]
-        ),
-        .executable(
-            name: "app",
-            targets: ["app"]
-        ),
         .executable(
             name: "codegen",
             targets: ["codegen"]
@@ -27,12 +19,6 @@ let package = Package(
             name: "AppModule",
             dependencies: [
                 .product(name: "ToyboxModule", package: "ToyboxPackage")
-            ]
-        ),
-        .executableTarget(
-            name: "app",
-            dependencies: [
-                .target(name: "AppModule")
             ]
         ),
         .executableTarget(
