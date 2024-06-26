02332d29e97ae76b318c877c49f1f8b551a3577f
Yuta Saito <kateinoigakukun@gmail.com>
2024-06-26 02:01:25 +0000

    Save build dir

--- repro.bash
+++ repro.bash
@@ -11,6 +11,8 @@ for flavor in direct indirect; do
   cp Debug.step1.swift ToyboxPackage/Sources/ToyboxCore/Debug.swift
   
   swift build -j 1 --package-path ./AppPackage -v | tee step1.$flavor.log
+
+  cp -R AppPackage/.build .build.$flavor
   
   # Source compatible change
   cp Debug.step2.swift ToyboxPackage/Sources/ToyboxCore/Debug.swift
